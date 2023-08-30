c3f70805f1a7989fac7b7dd9d27dc4ff6056fa07
Albert Rybalko <albert.rybalko@chulakov.ru>
2023-08-30 15:15:30 +0300

    feat: delete unnexessary AnyComponent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,17 @@
 import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
-import { appWithTranslation, useTranslation } from 'next-i18next';
+import { appWithTranslation } from 'next-i18next';
 
 import '../styles/global.css';
 import { AppLayout } from '@/config/layout';
 import { themeConfig } from '@/config/theme';
 
-const App = ({ Component, pageProps }: AppProps) => {
-	const AnyComponent = Component as any;
-	const { i18n } = useTranslation();
-	return (
-		<MantineProvider withGlobalStyles theme={themeConfig}>
-			<AppLayout>
-				<AnyComponent {...pageProps} />
-			</AppLayout>
-		</MantineProvider>
-	);
-};
+const App = ({ Component, pageProps }: AppProps) => (
+	<MantineProvider withGlobalStyles theme={themeConfig}>
+		<AppLayout>
+			<Component {...pageProps} />
+		</AppLayout>
+	</MantineProvider>
+);
 
 export default appWithTranslation(App);
