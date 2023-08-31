528a1be20b0f9508c594283b35cb06c32d41c2ae
Albert Rybalko <albert.rybalko@chulakov.ru>
2023-08-31 12:52:27 +0300

    feat: update form container

--- src/modules/Home/sections/Form/Form.tsx
+++ src/modules/Home/sections/Form/Form.tsx
@@ -7,6 +7,7 @@ import {
 	rem,
 	Text,
 	TextInput,
+	Title,
 } from '@mantine/core';
 import { hasLength, isEmail, isNotEmpty, useForm } from '@mantine/form';
 import { useTranslation } from 'next-i18next';
@@ -47,94 +48,99 @@ export const Form = () => {
 	});
 
 	return (
-		<Container fluid className={classes.container} id="contacts" bg={'#F4F7FD'}>
-			<Box
-				w={'100%'}
-				component="form"
-				onSubmit={form.onSubmit(() => {})}
-				className={classes.form}
-			>
-				<Text fz={40} lh={1.8} fw={400} mb={40}>
-					{t('title')}
-				</Text>
-				<Flex
-					direction={'column'}
-					align="center"
-					gap={10}
-					maw={800}
-					m="0 auto"
-					p={30}
-					sx={{
-						borderRadius: '30px',
-						boxShadow: '0px 0px 7px 2px #dadada',
-					}}
+		<Container fluid id="contacts" bg={'#F4F7FD'}>
+			<Container size="xl" pb={100} pt={100}>
+				<Box
+					w={'100%'}
+					component="form"
+					onSubmit={form.onSubmit(() => null)}
+					className={classes.form}
 				>
-					<TextInput
-						label={t('labelName')}
-						placeholder={t('placeholderName')}
-						withAsterisk
-						{...form.getInputProps('name')}
-						styles={{
-							wrapper: {
-								width: '300px',
-							},
-							input: {
-								height: '50px',
-								borderRadius: '8px',
-							},
-							label: {
-								paddingBottom: '10px',
-								fontSize: '16px',
-							},
+					<Title align="center" mb={24} order={2}>
+						{t('title')}
+					</Title>
+					<Text size={'xl'} maw={'775px'} mx={'auto'} mb={24}>
+						{t('description')}
+					</Text>
+					<Flex
+						direction={'column'}
+						align="center"
+						gap={10}
+						maw={800}
+						m="0 auto"
+						p={30}
+						sx={{
+							borderRadius: '30px',
+							boxShadow: '0px 0px 7px 2px #dadada',
 						}}
-					/>
-					<TextInput
-						maw={400}
-						label={t('labelCountry')}
-						placeholder={t('placeholderCountry')}
-						withAsterisk
-						mt="xl"
-						{...form.getInputProps('job')}
-						styles={{
-							wrapper: {
-								width: '300px',
-							},
-							input: {
-								height: '50px',
-								borderRadius: '8px',
-							},
-							label: {
-								paddingBottom: '10px',
-								fontSize: '16px',
-							},
-						}}
-					/>
-					<TextInput
-						maw={400}
-						label={t('labelEmail')}
-						placeholder={t('placeholderEmail')}
-						withAsterisk
-						mt="xl"
-						{...form.getInputProps('email')}
-						styles={{
-							wrapper: {
-								width: '300px',
-							},
-							input: {
-								height: '50px',
-								borderRadius: '8px',
-							},
-							label: {
-								paddingBottom: '10px',
-								fontSize: '16px',
-							},
-						}}
-					/>
-					<Button variant="gradient" type="submit" mt={40}>
-						{t('send')}
-					</Button>
-				</Flex>
-			</Box>
+					>
+						<TextInput
+							label={t('labelName')}
+							placeholder={t('placeholderName')}
+							withAsterisk
+							{...form.getInputProps('name')}
+							styles={{
+								wrapper: {
+									width: '300px',
+								},
+								input: {
+									height: '50px',
+									borderRadius: '8px',
+								},
+								label: {
+									paddingBottom: '10px',
+									fontSize: '16px',
+								},
+							}}
+						/>
+						<TextInput
+							maw={400}
+							label={t('labelCountry')}
+							placeholder={t('placeholderCountry')}
+							withAsterisk
+							mt="xl"
+							{...form.getInputProps('job')}
+							styles={{
+								wrapper: {
+									width: '300px',
+								},
+								input: {
+									height: '50px',
+									borderRadius: '8px',
+								},
+								label: {
+									paddingBottom: '10px',
+									fontSize: '16px',
+								},
+							}}
+						/>
+						<TextInput
+							maw={400}
+							label={t('labelEmail')}
+							placeholder={t('placeholderEmail')}
+							withAsterisk
+							mt="xl"
+							{...form.getInputProps('email')}
+							styles={{
+								wrapper: {
+									width: '300px',
+								},
+								input: {
+									height: '50px',
+									borderRadius: '8px',
+								},
+								label: {
+									paddingBottom: '10px',
+									fontSize: '16px',
+								},
+							}}
+						/>
+						<Button variant="gradient" type="submit" mt={40}>
+							{t('send')}
+						</Button>
+					</Flex>
+				</Box>
+			</Container>
 		</Container>
 	);
 };
